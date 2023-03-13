6b4e3f62363cf1734ae72bb062c3427993c7b18a
YuriyVA <yuriy.ramazanov@gmail.com>
2023-03-13 21:53:25 +0300

    1st

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите первое число  ");
+int num1 = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите второе число  "); 
+int num2 = int.Parse(Console.ReadLine()!);
+if (num1 >num2){
+    Console.WriteLine(num1);
+}
+else {
+    Console.Write(num2);
+}
