0e245c62f2aaab61bf2460f7a3d7c512c732bb06
YuriyVA <yuriy.ramazanov@gmail.com>
2023-03-13 21:54:44 +0300

    2nd

--- Program.cs
+++ Program.cs
@@ -8,3 +8,18 @@ if (num1 >num2){
 else {
     Console.Write(num2);
 }
+
+
+
+Console.WriteLine("Введите первое число  ");
+int num_1 = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите второе число  ");
+int num_2 = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите третье число  ");
+int num_3 = int.Parse(Console.ReadLine()!);
+
+int [] array_max = {num_1, num_2, num_3};
+
+Console.WriteLine(array_max.Max());
+
+
